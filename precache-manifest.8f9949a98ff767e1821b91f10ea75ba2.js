ef685eec1c67434cae85604d719a456c69c395e0
haymee <hmpark0502@gmail.com>
2020-06-07 17:49:31 +0900

    Updates

---
+++ precache-manifest.8f9949a98ff767e1821b91f10ea75ba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2104d83aae2a93d8b56d605b146b0a3",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "e15f2e6b8720682c0c02",
+    "url": "/movie_app_react/static/css/main.17ca7352.chunk.css"
+  },
+  {
+    "revision": "c3a0dee3bc39d93ae5d9",
+    "url": "/movie_app_react/static/js/2.92db7b6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_react/static/js/2.92db7b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e15f2e6b8720682c0c02",
+    "url": "/movie_app_react/static/js/main.8f241a1e.chunk.js"
+  },
+  {
+    "revision": "8c4f79154ab04262bbd6",
+    "url": "/movie_app_react/static/js/runtime-main.0f955f79.js"
+  }
+]);
